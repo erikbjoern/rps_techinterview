060139e9866473acf049870a351ee74c41db4c63
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-04 20:16:45 +0200

    successfully implementing player hotkeys and writing asserting tests

--- cypress/integration/playerInput.feature.js
+++ cypress/integration/playerInput.feature.js
@@ -4,32 +4,32 @@ describe('Player can use hotkeys for input', () => {
   })
   
   it('Left player can press "1" for "rock"', () => {
-    cy.get('body').type('1')
-    cy.get('#leftChoice').should('contain', 'rock')
+    cy.get('body').trigger('keydown', { keyCode: 49 })
+    cy.get('#leftChoice').should('have.value', 'rock')
   })
   
   it('Left player can press "2" for "paper"', () => {
-    cy.get('body').type('2')
-    cy.get('#leftChoice').should('contain', 'paper')
+    cy.get('body').trigger('keydown', { keyCode: 50 })
+    cy.get('#leftChoice').should('have.value', 'paper')
   })
   
   it('Left player can press "3" for "scissors"', () => {
-    cy.get('body').type('3')
-    cy.get('#leftChoice').should('contain', 'scissors')
+    cy.get('body').trigger('keydown', { keyCode: 51 })
+    cy.get('#leftChoice').should('have.value', 'scissors')
   })
   
-  it('Right player can press "1" for "rock"', () => {
-    cy.get('body').type('1')
-    cy.get('#rightChoice').should('contain', 'rock')
+  it('Right player can press "ArrowLeft" for "rock"', () => {
+    cy.get('body').trigger('keydown', { keyCode: 37 })
+    cy.get('#rightChoice').should('have.value', 'rock')
   })
   
-  it('Right player can press "2" for "paper"', () => {
-    cy.get('body').type('2')
-    cy.get('#rightChoice').should('contain', 'paper')
+  it('Right player can press "ArrowUp" for "paper"', () => {
+    cy.get('body').trigger('keydown', { keyCode: 38 })
+    cy.get('#rightChoice').should('have.value', 'paper')
   })
   
-  it('Right player can press "3" for "scissors"', () => {
-  cy.get('body').type('3')
-    cy.get('#rightChoice').should('contain', 'scissors')
+  it('Right player can press "ArrowRight" for "scissors"', () => {
+  cy.get('body').trigger('keydown', { keyCode: 39 })
+    cy.get('#rightChoice').should('have.value', 'scissors')
   })
 })
--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,33 @@ class App extends Component {
     this.setState({ winner: winner })
   }
 
+  componentDidMount(){
+    document.addEventListener('keydown', this.onKeyDownHandler)
+  }
+
+  onKeyDownHandler = e => {
+    switch (e.keyCode) {
+      case 49:
+        this.setState({ leftChoice: "rock" })
+        break
+      case 50:
+        this.setState({ leftChoice: "paper" })
+        break
+      case 51:
+        this.setState({ leftChoice: "scissors" })
+        break
+      case 37:
+        this.setState({ rightChoice: "rock" })
+        break
+      case 38:
+        this.setState({ rightChoice: "paper" })
+        break
+      case 39:
+        this.setState({ rightChoice: "scissors" })
+        break
+    }
+  }
+
 
     render() {
         return (
@@ -30,7 +57,6 @@ class App extends Component {
                 />
                 <DisplayWinner
                   winner={this.state.winner}
-                  debugger
                 />
             </div>
         )
