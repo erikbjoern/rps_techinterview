2b94cb7bee5145625d217cc1f69490b28ecb13b7
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-06 09:12:57 +0200

    writing feature test for visible feedback

---
+++ cypress/integration/visualFeedback.feature.js
@@ -0,0 +1,50 @@
+describe('Players should see', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('a button to start the game set', () => {
+    cy.get('#start-game').should('contain', 'Start!')
+  })
+
+  it('a timer countdown', () => {
+    cy.get('#start-game').click()
+    cy.get('#countdown').should('be.visible')
+  })
+
+  it("left player's choice after countdown", () => {
+    cy.get('#start-game').click()
+    cy.get('body').trigger('keydown', { keyCode: 49 })
+    cy.get('body').trigger('keydown', { keyCode: 39 })
+    cy.wait(5500)
+    cy.get('#leftPlayerChoice').should('be.visible')
+  })
+
+  it("right player's choice after countdown", () => {
+    cy.get('#start-game').click()
+    cy.get('body').trigger('keydown', { keyCode: 49 })
+    cy.get('body').trigger('keydown', { keyCode: 39 })
+    cy.wait(5500)
+    cy.get('#rightPlayerChoice').should('be.visible')
+  })
+  
+  it('the winner after countdown', () => {
+    cy.get('#start-game').click()
+    cy.get('body').trigger('keydown', { keyCode: 49 })
+    cy.get('body').trigger('keydown', { keyCode: 39 })
+    cy.wait(5500)
+    cy.get('#winner').should('contain', 'Right player wins')
+  })
+  
+  it("left player's score", () => {
+    cy.get('#leftPlayerScore').should('contain', '0')
+  })
+  
+  it("right player's score", () => {
+    cy.get('#rightPlayerScore').should('contain', '0')
+  })
+
+  it('the hotkeys to use', () => {
+    cy.get('#hotkeys').should('be.visible')
+  })
+})
