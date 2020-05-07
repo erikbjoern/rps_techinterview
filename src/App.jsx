824d886b929380db9527c838e7f76f69b9dd9621
Erik Björn <erik.g.bjoern@gmail.com>
2020-05-07 09:08:22 +0200

    player choice will be displayed as rock if no choice is made

--- src/App.jsx
+++ src/App.jsx
@@ -50,25 +50,27 @@ class App extends Component {
   }
 
   onKeyDownHandler = e => {
-    switch (e.keyCode) {
-      case 49:
-        this.setState({ leftChoice: "rock" })
-        break
-      case 50:
-        this.setState({ leftChoice: "paper" })
-        break
-      case 51:
-        this.setState({ leftChoice: "scissors" })
-        break
-      case 37:
-        this.setState({ rightChoice: "rock" })
-        break
-      case 38:
-        this.setState({ rightChoice: "paper" })
-        break
-      case 39:
-        this.setState({ rightChoice: "scissors" })
-        break
+    if(this.state.countdown > 1) {
+      switch (e.keyCode) {
+        case 49:
+          this.setState({ leftChoice: "rock" })
+          break
+        case 50:
+          this.setState({ leftChoice: "paper" })
+          break
+        case 51:
+          this.setState({ leftChoice: "scissors" })
+          break
+        case 37:
+          this.setState({ rightChoice: "rock" })
+          break
+        case 38:
+          this.setState({ rightChoice: "paper" })
+          break
+        case 39:
+          this.setState({ rightChoice: "scissors" })
+          break
+      }
     }
   }
 
@@ -98,8 +100,16 @@ class App extends Component {
 
     if (this.state.weHaveAWinner === true) {
       renderWinner = <Header as="h1" id="winner">{this.state.winner}</Header>
-      leftPlayer   = leftPlayerChoice
-      rightPlayer  = rightPlayerChoice
+      if (leftPlayerChoice) {
+        leftPlayer = leftPlayerChoice
+      } else {
+        leftPlayer = <Image size='tiny' src={leftRock}/>
+      }
+      if (rightPlayerChoice) {
+        rightPlayer = rightPlayerChoice
+      } else {
+        rightPlayer = <Image size='tiny' src={rightRock}/>
+      }
     } else {
       leftPlayer   = <Image size='tiny' src={leftRock}/>
       rightPlayer  = <Image size='tiny' src={rightRock}/>
